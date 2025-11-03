0fe6ecb23f74fb76630de40fdc7e1a1f4d7bc270
Oussama <Hassouneoussama8@gmail.com>
2025-11-03 23:12:06 +1100

    middleware auth path

--- ljm-website/app/utils/middleware.ts
+++ ljm-website/app/utils/middleware.ts
@@ -38,24 +38,17 @@ export async function updateSession(request: NextRequest) {
   const {
     data: { user },
   } = await supabase.auth.getUser();
+  const protectedPaths = ["/volunteerForm", "/confirmation", "/logged"];
 
-  // if (
-  //   !user &&
-  //   !request.nextUrl.pathname.startsWith("/")
-  //   // !request.nextUrl.pathname.startsWith("/error") &&
-  //   //     !request.nextUrl.pathname.startsWith("/")
-  // ) {
-  //   // no user, potentially respond by redirecting the user to the login page
-  //   const url = request.nextUrl.clone();
-  //   url.pathname = "/login";
-  //   return NextResponse.redirect(url);
-  // }
+  if (
+    !user &&
+    protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
 
-  // if (user && request.nextUrl.pathname.startsWith("/crew")) {
-  //   const url = request.nextUrl.clone();
-  //   url.pathname = "/logged";
-  //   return NextResponse.redirect(url);
-  // }
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
