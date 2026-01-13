90692ec5a04dd2d9c821a462a93f31595498e480
diana12301789 <12301789@students.koi.edu.au>
2026-01-13 20:12:31 +1100

    My profile updates and other small changes

--- ljm-website/app/(root)/(admin)/dashboard/layout.tsx
+++ ljm-website/app/(root)/(admin)/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/app/(root)/(admin)/components/dashboardNav/dash-sidebar";
 import "@/app/(root)/globals.css";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/sonner";
 
--- ljm-website/app/(root)/(users)/UserDashboard/layout.tsx
+++ ljm-website/app/(root)/(users)/UserDashboard/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
